d9b64ea42dc04b31a83eeabf9cd8e0177b8151b6
Kristin Ly <krily@ucdavis.edu>
2024-04-23 11:12:38 -0700

    toolkit 1

--- studio2/script.js
+++ studio2/script.js
@@ -14,22 +14,22 @@
 
     function showFoodInfo(point, data){
         const rating = ['like absolute rubbish!', 'boring & bland.', 'decent.', 'pretty good.', 'delicious!', 'like Gordon Ramsey made it!'];
-        // const foodImages = [
-        //     '<img src= "./images/day1.png" alt="food images" width="400" height="403">',
-        //     '<img src= "./images/day2.png" alt="food images" width="400" height="403">',
-        //     '<img src= "./images/day3.png" alt="food images" width="400" height="403">',
-        //     '<img src= "./images/day4.png" alt="food images" width="400" height="403">',
-        //     '<img src= "./images/day5.png" alt="food images" width="400" height="403">',
-        //     '<img src= "./images/day6.png" alt="food images" width="400" height="403">',
-        //     '<img src= "./images/day7.png" alt="food images" width="400" height="403">',
-        //     '<img src= "./images/day8.png" alt="food images" width="400" height="403">'
-        // ];
+        const foodImages = [
+            '<img src= "./images/day1.png" alt="food images" width="400" height="403">',
+            '<img src= "./images/day2.png" alt="food images" width="400" height="403">',
+            '<img src= "./images/day3.png" alt="food images" width="400" height="403">',
+            '<img src= "./images/day4.png" alt="food images" width="400" height="403">',
+            '<img src= "./images/day5.png" alt="food images" width="400" height="403">',
+            '<img src= "./images/day6.png" alt="food images" width="400" height="403">',
+            '<img src= "./images/day7.png" alt="food images" width="400" height="403">',
+            '<img src= "./images/day8.png" alt="food images" width="400" height="403">'
+        ];
 
         
         document.querySelector('#time').innerHTML = data[point].time;
         document.querySelector('#meal').innerHTML = `I ate ${data[point].food}.`;
 
-        // document.querySelector('#foodimages').innerHTML = foodImages[point];
+        document.querySelector('#foodimages').innerHTML = foodImages[point];
 
 
         document.querySelector('#statement').innerHTML = `I rate it a ${data[point].review} out of 5 because it tasted ${rating[data[point].review]}`;
